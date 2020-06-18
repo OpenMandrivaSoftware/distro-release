68ca98cb5e0284f88817694a83227a7b83a1e39e
tpgxyz <tpgxyz@gmail.com>
2020-06-18 16:54:49 +0200

    fir it for good

--- rpm/update_macros.sh
+++ rpm/update_macros.sh
@@ -1,4 +1,4 @@
-rm -f $(pwd)/lua/common.lua $(pwd)/lua/forge.lua $(pwd)/build/macros.d/macros.forge
-wget https://src.fedoraproject.org/cgit/rpms/redhat-rpm-config.git/plain/common.lua -O $(pwd)/lua/common.lua
-wget https://src.fedoraproject.org/cgit/rpms/redhat-rpm-config.git/plain/forge.lua -O $(pwd)/lua/forge.lua
+rm -f $(pwd)/build/fedora/common.lua $(pwd)/build/fedora/forge.lua $(pwd)/build/macros.d/macros.forge
+wget https://src.fedoraproject.org/cgit/rpms/redhat-rpm-config.git/plain/common.lua -O $(pwd)/build/fedora/common.lua
+wget https://src.fedoraproject.org/cgit/rpms/redhat-rpm-config.git/plain/forge.lua -O $(pwd)/build/fedora/forge.lua
 wget https://src.fedoraproject.org/cgit/rpms/redhat-rpm-config.git/plain/macros.forge -O $(pwd)/build/macros.d/macros.forge
