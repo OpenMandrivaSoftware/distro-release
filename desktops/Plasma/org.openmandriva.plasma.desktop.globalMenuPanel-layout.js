926fba2e6f09d888e45e4f9e1ddac85aa4937054
rugyada <rugyada@gmail.com>
2022-09-03 18:01:57 +0200

    sync with rome branch

--- desktops/Plasma/org.openmandriva.plasma.desktop.globalMenuPanel-layout.js
+++ desktops/Plasma/org.openmandriva.plasma.desktop.globalMenuPanel-layout.js
@@ -8,8 +8,8 @@ print("Loading OpenMandriva Plasma panel configuration")
 // remove already existing old panels
 function removeOldPanels()
 {
-	while(panelIds.length)
-		panelById(panelIds[0]).remove()
+    while(panelIds.length)
+	panelById(panelIds[0]).remove()
 }
 
 // remove already existing other panels
@@ -81,7 +81,7 @@ var langIds = ["as",    // Assamese
                "zh_TW"] // Traditional Chinese
 
 if (langIds.indexOf(languageId) != -1) {
-	panel.addWidget("org.kde.plasma.kimpanel");
+    panel.addWidget("org.kde.plasma.kimpanel");
 }
 
 var systray = panel.addWidget("org.kde.plasma.systemtray")
@@ -92,9 +92,9 @@ systray.writeConfig("ShowCommunications","true")
 systray.writeConfig("systemServicesShown","true")
 systray.writeConfig("hardwareControlShown","true")
 systray.writeConfig("miscellaneousShown","true")
-systray.writeConfig("extraItems", "org.kde.plasma.devicenotifier,org.kde.plasma.notifications,org.kde.plasma.bluetooth,org.kde.plasma.battery,org.kde.plasma.volume,org.kde.plasma.networkmanagement,org.kde.muonnotifier,org.kde.plasma.clipboard")
+systray.writeConfig("extraItems", "org.kde.plasma.devicenotifier,org.kde.plasma.notifications,org.kde.plasma.bluetooth,org.kde.plasma.battery,org.kde.plasma.volume,org.kde.plasma.networkmanagement,org.kde.plasma.clipboard")
 systray.writeConfig("hiddenItems", "hp-systray,python3.4m")
-systray.writeConfig("knownItems", "org.kde.plasma.notifications,org.kde.plasma.bluetooth,org.kde.plasma.clipboard,org.kde.plasma.battery,org.kde.plasma.volume,org.kde.plasma.networkmanagement,org.kde.plasma.mediacontroller,org.kde.muonnotifier,org.kde.plasma.devicenotifier,org.kde.plasma.clipboard")
+systray.writeConfig("knownItems", "org.kde.plasma.notifications,org.kde.plasma.bluetooth,org.kde.plasma.clipboard,org.kde.plasma.battery,org.kde.plasma.volume,org.kde.plasma.networkmanagement,org.kde.plasma.mediacontroller,org.kde.plasma.devicenotifier,org.kde.plasma.clipboard")
 
 var clock = panel.addWidget("org.kde.plasma.digitalclock")
 clock.currentConfigGroup = ["Appearance"]
--- desktops/Plasma/plasma-firstsetup.sh
+++ desktops/Plasma/plasma-firstsetup.sh
@@ -4,26 +4,11 @@ if [ -n "$(command -v kreadconfig5)" ]; then # Check if Plasma is installed, oth
     FIRSTRUN=$(kreadconfig5 --group "OpenMandriva" --key "FirstRun" --default "true")
 
     if [ "$FIRSTRUN" = 'true' ]; then
-# (tpg) enable xscreensaver
-	if [ ! -f "~/.config/autostart/xscreensaver.desktop" -a -x "$(command -v xscreensaver)" ]; then
-	    mkdir -p "~/.config/autostart"
-	    cat > "~/.config/autostart/xscreensaver.desktop" << "EOF"
-[Desktop Entry]
-Name=XScreenSaver
-Exec=xscreensaver -nosplash
-Icon=xscreensaver
-Terminal=False
-Type=Application
-X-KDE-StartupNotify=False
-OnlyShowIn=KDE;
-EOF
-	fi
-
 # (tpg) add special icons on DESKTOP
 	USER_DESKTOP="$(xdg-user-dir DESKTOP)"
 	for i in om-welcome join donate calamares; do
 	    if [ ! -e "$USER_DESKTOP"/$i.desktop ] && [ -e /usr/share/applications/$i.desktop ]; then
-		cp -f /usr/share/applications/$i.desktop "$USER_DESKTOP" 2> /dev/null
+		cp -f /usr/share/applications/$i.desktop "$USER_DESKTOP"/$i.desktop 2> /dev/null
 		chmod +x "$USER_DESKTOP"/$i.desktop ||:
 	    fi
 	done
