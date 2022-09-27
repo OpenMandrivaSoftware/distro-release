fe2daaa6f1fdb6aeef7b8ad0f523046e8996c15a
rugyada <46781309+rugyada@users.noreply.github.com>
2022-09-27 10:00:57 +0200

    Update org.openmandriva.plasma.desktop.defaultPanel-layout.js

--- desktops/Plasma/org.openmandriva.plasma.desktop.defaultPanel-layout.js
+++ desktops/Plasma/org.openmandriva.plasma.desktop.defaultPanel-layout.js
@@ -32,8 +32,8 @@ var launcher = panel.addWidget("org.kde.plasma.kicker")
 launcher.currentConfigGroup = ["Shortcuts"]
 launcher.writeConfig("global", "Alt+F1")
 launcher.currentConfigGroup = ["General"]
-// launcher.writeConfig("favoriteApps", "preferred://browser,org.kde.kmail.desktop,kcm_kdeconnect.desktop,org.kde.dolphin.desktop,org.kde.kate.desktop,org.kde.konsole.desktop,systemsettings.desktop")
 launcher.writeConfig("favoriteApps", "preferred://browser,systemsettings.desktop,org.kde.dolphin.desktop,org.kde.konsole.desktop,org.kde.kwrite.desktop")
+launcher.writeConfig("favoriteSystemActions", "logout")
 launcher.writeConfig("limitDepth", "false")
 launcher.writeConfig("useExtraRunners", "true")
 launcher.writeConfig("alignResultsToBottom", "true")
