3a717c61c2229703dc63ea1e067a29055e387a73
rugyada <rugyada@gmail.com>
2023-09-15 10:27:45 +0200

    add layouts

---
+++ desktops/Plasma6/org.openmandriva6.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -0,0 +1,6 @@
+loadTemplate("org.openmandriva.plasma.desktop.defaultPanel")
+
+var desktopsArray = desktopsForActivity(currentActivity());
+for( var j = 0; j < desktopsArray.length; j++) {
+    desktopsArray[j].wallpaperPlugin = 'org.kde.image';
+}
